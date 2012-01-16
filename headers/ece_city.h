82e2da5aae8901dbed8bfa85df66223df3aaee89
MaximeJUMELINE <maxime.jumeline@gmail.com>
2012-01-16 20:31:23 +0100

    Update headers/ece_city.h

--- headers/ece_city.h
+++ headers/ece_city.h
@@ -1,58 +1,61 @@
 /**
     ECE_CITY 2012
     ece_city.h
-    © Jean Guyomarc'h — Maxime Jumeline — Xavier Brun
+    ï¿½ Jean Guyomarc'h ï¿½ Maxime Jumelineï¿½ï¿½ Xavier Brun
 **/
 
 #ifndef ECE_CITY_H_INCLUDED
 #define ECE_CITY_H_INCLUDED
 
 /**
-    CE FICHIER CONTIENT L'INTÉGRALITÉ DES PROTOTYPES
-    DES FONCTIONS DÉVELOPPÉES POUR CE PROJET, OU ÉTANT
-    DES RÉÉCRITURES DE FONCTIONS DÉJÀ EXISTANTES DANS
-    LES BIBLIOTHÈQUES STANDARD.
+    CE FICHIER CONTIENT L'INTï¿½GRALITï¿½ DES PROTOTYPES
+    DES FONCTIONS Dï¿½VELOPPï¿½ES POUR CE PROJET, OU ï¿½TANT
+    DES Rï¿½ï¿½CRITURES DE FONCTIONS Dï¿½Jï¿½ EXISTANTES DANS
+    LES BIBLIOTHï¿½QUES STANDARD.
 **/
 
 
 /*************************
 FONCTIONS D'INITIALISATION
 *************************/
-void jg_allegro_init(void);                    // Initialise pleinement Allegro, et de manière sécurisée
-_generale* jg_init(void);                      // Initialise la structure générale
+void jg_allegro_init(void);                    // Initialise pleinement Allegro, et de maniï¿½re sï¿½curisï¿½e
+_generale* jg_init(void);                      // Initialise la structure gï¿½nï¿½rale
 void jg_font_init(_generale *);                // Chargement de la police
 
 /*********
 SURCOUCHES
 *********/
-BITMAP* jg_create_bitmap(int, int);         // Créée une BITMAP de manière totalement sécurisée
-BITMAP* jg_load_bitmap(const char *);       // Charge une BITMAP de manière sécurisée
-void* jg_malloc(size_t);                    // Allocation de mémoire sécurisée
+BITMAP* jg_create_bitmap(int, int);         // Crï¿½ï¿½e une BITMAP de maniï¿½re totalement sï¿½curisï¿½e
+BITMAP* jg_load_bitmap(const char *);       // Charge une BITMAP de maniï¿½re sï¿½curisï¿½e
+void* jg_malloc(size_t);                    // Allocation de mï¿½moire sï¿½curisï¿½e
 
 /*****************************
 FONCTION DE DESSIN DE LA CARTE
 *****************************/
 void jg_draw_grid(_generale *, int, int, int);      // Dessine la grille de terrain
-void jg_zoom_controller(_generale *);               // Répondeur d'événement de zoom
-void jg_scroll_controller(_generale *);             // Répondeur d'événement de scroll
+void jg_zoom_controller(_generale *);               // Rï¿½pondeur d'ï¿½vï¿½nement de zoom
+void jg_scroll_controller(_generale *);             // Rï¿½pondeur d'ï¿½vï¿½nement de scroll
 
 /********************************
-CONTRÔLEURS DE LA BARRE DES MENUS
+CONTRï¿½LEURS DE LA BARRE DES MENUS
 ********************************/
-void jg_menu_controller(_generale *);           // Répondeur d'événement du menu
+void jg_menu_controller(_generale *);           // Rï¿½pondeur d'ï¿½vï¿½nement du menu
 void jg_draw_menu(_generale *);                 // Dessine le menu initial
 
 /**********************
-CONTRÔLEURS D'AFFICHAGE
+CONTRï¿½LEURS D'AFFICHAGE
 **********************/
-void jg_display_all(_generale *);                   // Affiche à l'écran
+void jg_display_all(_generale *);                   // Affiche ï¿½ l'ï¿½cran
 void jg_display_menu(_generale *);                  // Colle le menu sur le BUFFER !!
 void jg_display_menuitem(_generale *);              // Colle les bouts de menus sur le BUFFER DU MENU !!
 void jg_display_toolbox(_generale *, int, int);     // Recompose la toolbox sur le BUFFER DE LA TOOLBOX
-void jg_reload_all(_generale *);                    // Redessine la totalité des acteurs graphiques
+void jg_reload_all(_generale *);                    // Redessine la totalitï¿½ des acteurs graphiques
+void mj_display_info(_generale *);                  // Appel de la page d'information
+void mj_display_load(_generale *);                  // Appel la fonction de chargement d'une partie
+void mj_display_save(_generale *);                  // Appel la fonction de sauvegarde d'une partie
 
 /*************************************
-RESPONSABLE DES CHARGEMENTS DE DONNÉES
+RESPONSABLE DES CHARGEMENTS DE DONNï¿½ES
 *************************************/
 void jg_load_menu(_generale *);                 // Charge les images du menu
 void jg_load_toolbox(_generale *);              // Charge les images de la toolbox
@@ -61,9 +64,9 @@ void jg_load_tiles(_generale *);                // Charge les tuiles
 /*******
 TOOLBOX
 *******/
-void jg_toolbox_controller(_generale *);        // Répondeur d'événements de la toolbox aux clics
+void jg_toolbox_controller(_generale *);        // Rï¿½pondeur d'ï¿½vï¿½nements de la toolbox aux clics
 void jg_draw_toolbox(_generale *);              // Redessine la toolbox originale
-void jg_drag_toolbox(_generale *);              // Répondeur d'événement de la toolbox au Drag And Drop
+void jg_drag_toolbox(_generale *);              // Rï¿½pondeur d'ï¿½vï¿½nement de la toolbox au Drag And Drop
 
 /*****************
 DESSINE LES TUILES
